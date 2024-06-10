6e0bd4e570810632fa792b6133ddc08fc2c98eda
Albert Lima <137114275+Albert-Lima@users.noreply.github.com>
2024-06-10 10:57:54 -0300

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,22 @@
+const dataFinal = new Date().getTime() + 5000;
+
+const intervalo = setInterval(function() {
+  const dataAtual = new Date().getTime();
+
+  const diferenca = dataFinal - dataAtual;
+
+  const segundos = Math.floor(diferenca / 1000);
+
+  const contadorElemento = document.getElementById('countdown');
+  contadorElemento.innerHTML = ` ${segundos}`;
+
+  if (diferenca <= 0) {
+    clearInterval(intervalo); 
+    contadorElemento.innerHTML = '0';
+  }
+}, 1000);
+
+// Redireciona para a página especificada após 5 segundos
+setTimeout(function() {
+  window.location.href = "https://wa.me/5585989390143?text=Olá, vim pelo Google. Gostaria de fazer um orçamento!";
+}, 5000); // 5000 milissegundos = 5 segundos
